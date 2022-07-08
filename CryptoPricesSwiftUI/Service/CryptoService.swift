22bcb7eee3547642d13e53dcfa32b8502187ee98
ergeakova <ergeak@gmail.com>
2022-07-09 02:04:59 +0300

    added crypto service

---
+++ CryptoPricesSwiftUI/Service/CryptoService.swift
@@ -0,0 +1,46 @@
+//
+//  CryptoService.swift
+//  CryptoPricesSwiftUI
+//
+//  Created by Erge Gevher Akova on 9.07.2022.
+//
+
+import Foundation
+
+class CryptoService {
+    
+    func downloadCurrencies(url: URL, key: String, completion: @escaping(Result<[CryptoCurrency]?, DownloaderError>) -> Void){
+        
+        var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.setValue(key, forHTTPHeaderField: "x")
+           
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let error = error{
+                print(error.localizedDescription)
+                completion(.failure(.badURL))
+            }
+            
+            guard let data = data, error == nil else {
+                return completion(.failure(.noData))
+            }
+                
+            guard let currencies = try? JSONDecoder().decode([CryptoCurrency].self,from: data) else {
+                return completion(.failure(.dataParserError))
+            }
+            
+            print(currencies)
+            completion(.success(currencies))
+        }.resume()
+    }
+    
+    
+}
+
+enum DownloaderError: Error {
+    case badURL
+    case noData
+    case dataParserError
+}
