f21515ac5bbc845b0ea700572ae4c79a38cb1773
ergeakova <ergeak@gmail.com>
2022-07-08 21:40:34 +0300

    Initial Commit

---
+++ CryptoPricesSwiftUI/CryptoPricesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPricesSwiftUIApp.swift
+//  CryptoPricesSwiftUI
+//
+//  Created by Erge Gevher Akova on 8.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPricesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
